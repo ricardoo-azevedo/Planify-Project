61f6ee4eeddd8b1e97e22b5d6fef3c1f442289bf
João Paulo <jpsilvaprogramador@gmail.com>
2024-05-12 12:42:00 -0300

    Criação do arquivo .gitignore, Atualização do Diagrama de Classes, criação da pasta Conexao, alteração no nome da pasta Pages para Templates e  exclusão da pasta Planify2

--- planify/index.php
+++ planify/index.php
@@ -1,28 +1,50 @@
 <!doctype html>
 <html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Bootstrap demo</title>
-    <meta content="" name="description">
-    <meta content="" name="keyworks">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Arquivo CSS -->
-    <link rel="stylesheet" href="style.css">
+  <title>Bootstrap demo</title>
+  <meta content="" name="description">
+  <meta content="" name="keyworks">
 
-    <!-- Bootstrap CSS CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <!-- Arquivo CSS -->
+  <link rel="stylesheet" href="style.css">
 
-  </head>
+  <!-- Bootstrap CSS CDN -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-  <body>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
-    
-    <!-- =========== Main ===========  --> 
-    
+</head>
 
-    <!-- Bootstrap JS CDN -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
+<body>
+
+  <header class="header ">
+    <div class="bg-info">
+      <?php
+      include_once 'assets/templates/header.html';
+      include_once 'assets/templates/herosection.html';
+      ?>
+    </div>
+  </header>
+
+
+
+  <!-- =========== Main ===========  -->
+
+  <footer class="footer bg-warning">
+    <div class="bg-danger">
+      <?php
+      include_once 'assets/templates/footer.html';
+      ?>
+    </div>
+  </footer>
+
+
+  <!-- Bootstrap JS CDN -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
 </html>
